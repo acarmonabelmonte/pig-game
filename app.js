284af9ca906334db5fb9e6de013115bb96cc5d0f
acarmonabelmonte <acarmonabelmonte@gmail.com>
2019-11-05 14:53:41 +0100

    Events and Event Handling: Rolling the

--- app.js
+++ app.js
@@ -14,8 +14,23 @@ scores = [0, 0];
 roundScores = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6);
-console.log(dice)
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    
+    // Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+
+    // Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // Update the score if the rolled number is not a 1
+
+
+});
